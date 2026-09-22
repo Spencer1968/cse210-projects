bec7e08aca6561a9bf3d6ff39a6ce375d0637a3c
Spencer Earl <iamspencerearl@gmail.com>
2026-09-22 10:31:56 -0600

    Finished Prep1 and Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string gradeString;
+        Console.Write("What is your grade? (1-100) ");
+        gradeString = Console.ReadLine();
+
+        int gradeInt = int.Parse(gradeString);
+
+        if (gradeInt >= 90)
+            Console.WriteLine("You have earned an A!");
+        else if (gradeInt >= 80 && gradeInt < 90)
+            Console.WriteLine("You have earned a B!");
+        else if (gradeInt >= 70 && gradeInt < 80)
+            Console.WriteLine("You have earned a C.");
+        else if (gradeInt >= 60 && gradeInt < 70)
+            Console.WriteLine("You have earned a D.");
+        else if (gradeInt < 60)
+            Console.WriteLine("You earned an F.");
+
+        if (gradeInt >= 70)
+            Console.WriteLine("Congratulations! You passed!");
+        else if (gradeInt < 70)
+            Console.WriteLine("Sorry, you have failed.");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,8 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Bonjour tout le monde.");
-        Console.WriteLine("Hey Jude.");
-        Console.WriteLine("Hey Jay");
+        int x = 20;
+        int y = 30;
+        int z = 40;
+
+        if (x == 10 && y == 30 || z == 30) {
+            Console.WriteLine("X is 10");
+            Console.WriteLine("Y is fun");
+        }
+        else if (x == 20)
+        {
+            Console.WriteLine("X is 20");
+        }
+        else
+        {
+            Console.WriteLine("Default output");
+        }
     }
 }
